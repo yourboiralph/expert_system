8d97530e12eb9558264b2271d95f2d447f640077
SCJAB <zzep96@gmail.com>
2023-11-01 13:28:17 +0800

    update

--- components/visibility.php
+++ components/visibility.php
@@ -3,23 +3,35 @@ session_start();
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (isset($_POST['visibility'])) {
-        $visibility = $_POST['visibility']; // Storing the choice in a variable
+        $visibility = $_POST['visibility'];
 
         if ($visibility == 'Skip') {
-            $_SESSION['visibility'] = 'private'; // Storing the choice in the session
+            // Set session visibility to 'private' and redirect
+            $_SESSION['visibility'] = 'private';
             header("location: questions_answerChoices.php");
-            exit(); // Add an exit after the header to prevent further execution
+            exit(); // Ensure exit after redirection
         } elseif ($visibility == 'Submit') {
             $firstname = $_POST['firstname'];
             $lastname = $_POST['lastname'];
             $age = $_POST['age'];
 
             if (empty($firstname) || empty($lastname) || empty($age)) {
-                echo 'Please fill in all the required fields (First name, Last name, Age) before submitting.';
-            } else {
+                echo '<div class="absolute top-28 inset-x-[20%] md:inset-x-[38%] md:inset-y-[100%] md:top-24 md:transform">
+                    <h3 style="color:red" class="bg-white rounded-full text-center text-xs md:font-medium p-2 z-50">Please fill in all the required fields (First name, Last name, Age) before submitting.</h3>
+                </div>';
+            } elseif ($age < 10) {
+                echo '<div class="absolute top-28 inset-x-[20%] md:inset-x-[38%] md:inset-y-[100%] md:top-24 md:transform">
+                    <h3 style="color:red" class="bg-white rounded-full text-center text-xs md:font-medium p-2 z-50">Age must be 10 or greater.</h3>
+                </div>';
+            } elseif ($age <= 90) {
+                // Set session visibility to 'public' and redirect
                 $_SESSION['visibility'] = 'public';
                 header("location: questions_answerChoices.php");
-                exit(); // Add an exit after the header to prevent further execution
+                exit();
+            } else {
+                echo '<div class="absolute top-28 inset-x-[20%] md:inset-x-[38%] md:inset-y-[100%] md:top-24 md:transform">
+                    <h3 style="color:red" class="bg-white rounded-full text-center text-xs md:font-medium p-2 z-50">Age must be 90 or less.</h3>
+                </div>';
             }
         }
     }
@@ -27,40 +39,59 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 ?>
 
 <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="../dist/output.css">
-        <title>Get Started</title>
-    </head>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../dist/output.css">
+    <title>Get Started</title>
+</head>
 <body class="bg-image flex justify-center items-center h-screen">
-    <div class="bg-black bg-opacity-60 p-4 rounded-lg shadow-md">
-        <form method="post" action="">
-            <h1 class="text-4xl font-bold text-white mb-5">Before we begin<span class="text-base flex text-white font-thin">please choose whether you would like to proceed <br>anonymously or provide your information.</span></h1>
-            <div class="w-64 mx-auto font-bold text-white">
-                <label for="firstname">First name</label><br>
-                <input type="text" id="firstname" name="firstname" class="w-62 px-4 mt-1 rounded-lg border-2 border-yellow-500 bg-transparent"><br>
-                <label for="lastname">Last name</label><br>
-                <input type="text" id="lastname" name="lastname" class="w-62 px-4 mt-1 rounded-lg border-2 border-yellow-500 bg-transparent"><br>
-                <label for="age">Age</label><br>
-                <input type="text" id="age" name="age" class="w-16 px-4 mt-1 rounded-lg border-2 border-yellow-500 bg-transparent"><br>
-            </div>
-            <div class="mt-4 flex justify-end space-x-5">
-                <input type="submit" name="visibility" value="Submit" class="p-2 rounded-lg cursor-pointer hover:bg-opacity-40 hover:bg-yellow-500 hover:text-white hover:scale-110">
-                <input type="submit" name="visibility" value="Skip" class="p-2 rounded-lg cursor-pointer">
-            </div>
-        </form>
-    </div>
+<div class="bg-black bg-opacity-60 p-4 rounded-lg shadow-md">
+    <form method="post" action="">
+        <h1 class="text-4xl font-bold text-white mb-5">Before we begin<span class="text-base flex text-white font-thin">please choose whether you would like to proceed anonymously <br>or provide your information.</span></h1>
+        <div class="w-64 mx-auto font-bold text-white">
+            <label for="firstname">First name</label><br>
+            <input type="text" id="firstname" name="firstname" class="w-62 px-4 mt-1 rounded-lg border-2 border-yellow-500 bg-transparent" oninput="validateInput(this)"><br>
+            <label for="lastname">Last name</label><br>
+            <input type="text" id="lastname" name="lastname" class="w-62 px-4 mt-1 rounded-lg border-2 border-yellow-500 bg-transparent" oninput="validateInput(this)"><br>
+            <label for="age">Age</label><br>
+            <input type="number" id="age" name="age" value="0" class="w-32 px-4 mt-1 rounded-lg border-2 border-yellow-500 bg-transparent" oninput="validateAge(this)"><br>
+        </div>
+        <div class="mt-4 flex justify-end space-x-5">
+            <input type="submit" name="visibility" value="Submit" class="p-2 rounded-lg cursor-pointer text-white hover:bg-opacity-40 hover:bg-yellow-500 hover:scale-110">
+            <input type="submit" name="visibility" value="Skip" class="p-2 rounded-lg cursor-pointer text-slate-400 hover:bg-opacity-40 hover:bg-slate-400 hover:scale-110">
+        </div>
+    </form>
+</div>
 
-        <style>
-            body {
-                background-image: url('../img/visibility.jpg');
-                background-size: cover;
-                background-position: center;
-                background-attachment: fixed;
-                background-repeat: no-repeat;
-            }
-        </style>
-    </body>
+<style>
+    body {
+        background-image: url('../img/visibility.jpg');
+        background-size: cover;
+        background-position: center;
+        background-attachment: fixed;
+        background-repeat: no-repeat;
+    }
+</style>
+
+<script>
+    function validateInput(inputField) {
+        if (inputField.value.trim() !== "") {
+            inputField.style.borderColor = "green";
+        } else {
+            inputField.style.borderColor = "red";
+        }
+    }
+
+    function validateAge(ageField) {
+        const age = parseInt(ageField.value);
+        if (age <= 10 || age > 90) {
+            ageField.style.borderColor = "red";
+        } else {
+            ageField.style.borderColor = "green";
+        }
+    }
+</script>
+</body>
 </html>
